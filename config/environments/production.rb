8f9b7bc4fdb9b73c90800e715240e237e4fffdc3
Enbake <enbake@enbake-Aspire-E1-530.(none)>
2014-05-31 16:02:14 +0530

    Updated code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
+  config.action_mailer.default_url_options = { :host => 'http://test-app-10.herokuapp.com/' } 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  ActionMailer::Base.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
      :authentication => :plain,
      :address => "smtp.mailgun.org",
